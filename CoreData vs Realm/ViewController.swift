8b8402423443b27c1b2fe066d752191c3135ab20
Faizan Memon <faizan@sharechat.co>
2022-09-20 14:48:07 +0530

    Initial Commit

---
+++ CoreData vs Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData vs Realm
+//
+//  Created by Faizan Memon on 20/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
